53a0abb95c519c2d60f553a4953b84f90196eee7
Priyanshu <priyanshugangwar951@gmail.com>
2026-03-20 14:26:14 +0530

    first commit

---
+++ Week 1/script.js
@@ -0,0 +1,148 @@
+// Sum of two integers
+// let a = 10
+// let b = 30
+// console.log(a+b)
+         
+
+// let a = 45
+// let b = 17
+// console.log(`The Sum of ${a} & ${b} = ${a+b}`)
+
+// let age = 12
+// let name = "Shery"
+// console.log(`Hello ${name}, you are ${age} old`)
+
+// let x = 5
+// console.log(x++,x) // after increement
+// console.log(++x) //  imidatly or before increment
+// console.log(x--,x)
+// console.log(--x,x)
+
+
+// let x = 2  // 5 
+// let y = 3   // 2
+// x = x+y //5
+// y = x-y // 5-3 = 2
+// x = x-y   // 5-2 = 3
+// console.log(x)
+// console.log(y)
+
+// let length = 10
+// let width  = 10
+// // Area=Length×Width // ayat
+// // Perimeter=2×(Length+Width)
+// console.log(`Area of rectangle : ${length*width}`)
+// console.log(`Area of rectanlge : ${2*length+width}`)
+
+// A=P×(1+100R​)y
+
+// let  p = 1000
+// let  r = 10 // %
+// let  y = 2
+// console.log(` A = ${Math.pow(p*1+r/100,y)}`)
+
+
+
+// s= a+b+c/2
+// A=s⋅(s−a)⋅(s−b)⋅(s−c)
+
+// let a = 5
+// let b = 6 
+// let c = 7
+// let s = (a+b+c)/2
+// console.log(s)
+// console.log(`Area = ${Math.sqrt(s*(s-a)*(s-b)*(s-c))}`)
+
+
+// circle =  2*pi*r
+// area =  pi*r*r
+
+// let r = 2
+// console.log(`Circusium =  ${2*Math.PI*r}`)
+// console.log(`Area = ${Math.PI*Math.pow(r,r)}`)
+
+// let x  = 10
+// let y = 20
+
+// if(x>y){
+//   console.log( x," is greater than ", y)
+// }else{
+//     console.log(y,"is grater than ", x)
+// }
+
+// let gender = prompt("Enter your gender")
+
+// if(gender==="male"){
+//     console.log("Good morning sir")
+// }else if(gender === "female"){
+//    console.log("Good morning mam")
+// }
+// else{
+//      console.log(`good morning  ${gender}`)
+// }
+
+// let gender =  prompt("Enter Your Gender")
+//  console.log( gender.toLowerCase() === "male" ? "Good Morning sir" : gender.toLowerCase() === "female" ? "Good morning mam" :" Enter valid gender")
+
+
+// let number = Number(prompt("Enter A number"))
+// console.log(number%2===0?"Number is  Even ": "number is odd ")
+
+// let Name = prompt("Enter your name")
+// let age =  prompt("Enter Your age")
+// console.log(age>=18?`${name} is valid for vote`: ` ${Name} is not valid for vote he is eligible for ${18-age} years `)
+
+// let days = Number(prompt("Enter day"))
+
+// switch(days){
+//     case 1 :
+//         console.log("Sunday")
+//    break
+
+//    case 2:
+//     console.log("Monday")
+//     break
+
+//     case 3 :
+//     console.log("Tuesday")
+//     break
+
+//     case 4 :
+//     console.log("thursday")
+//     break
+//       case 5 :
+//     console.log("wednesday")
+//     break
+//       case 6 :
+//     console.log("friday")
+//     break
+//      case 7 :
+//     console.log("saturday")
+//     break
+
+//     default :
+//     console.log("Please enter valid day")
+// }
+
+// let a = Number(prompt("Enter a first number "))
+// let b = Number(prompt("Enter a second number "))
+// let c = Number(prompt("Enter a third number "))
+
+// console.log(Math.max(a,b,c))
+
+
+// let year = prompt("Enter a year to find a leap year")
+// console.log((year%4 === 0) && (year%100 !==0 ) || (year%400 === 0)? `This is leap year ${year}`:`This is not leap year ${year}` )
+
+// let price = prompt("Enter a price")
+// let dis ;
+
+//  price>1000 ? dis = 50 : price>450? dis = 30 : dis = 10
+
+// let discount  = (price*dis)/100
+// let finalbill  = price-discount
+
+// console.log(finalbill)
+
+
+
